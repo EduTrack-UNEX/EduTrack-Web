30aff216ca9cf3909d8a4f76c3fe83ada290cff1
Italo-Yan <italoyandev@gmail.com>
2025-08-21 23:30:50 -0300

    feat: starting the project and configuring

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-9xl font-bold text-blue-400">EduTrack</h1>
+      <p className="text-4xl font-bold text-green-600">Tamo rodando...</p>
+    </div>
+  )
+}
