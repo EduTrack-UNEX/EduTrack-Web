2a55cb40fb08cb459e6c7d76769f4074a6bd0664
Italo-Yan <italoyandev@gmail.com>
2025-10-16 20:57:59 -0300

    feat: criando a tela de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-
-import Login from './pages/login'; 
-import AdcDisciplina from './pages/AdcDisciplina'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import AdcDisciplina from "./pages/AdcDisciplina"
+import Login from "./pages/login"
+import Cadastro from "./pages/cadastro"
 
 function App() {
   return (
-    <div className="App">
-      <AdcDisciplina /> 
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdcDisciplina />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
